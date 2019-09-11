f76d3e6a362d4333726f56de463b867ec9403429
KelyX <kyle.hagad@gmail.com>
2019-09-10 21:36:42 -0600

    skip & slider functions

--- 11-HTMLodean/scripts.js
+++ 11-HTMLodean/scripts.js
@@ -4,13 +4,18 @@ const progress = videoPort.querySelector('.progress');
 const progressBar = videoPort.querySelector('.progress__filled');
 const playbackToggle = videoPort.querySelector('.toggle');
 const skipButtons = videoPort.querySelectorAll('[data-skip]');
-const sliders = videoPort.querySelectorAll('player__slider');
+const sliders = videoPort.querySelectorAll('.player__slider');
 
-function playPause() { video[video.paused ? 'play' : 'pause'](); } // easy version-> `video.paused ? video.play() : video.pause()`
-function updateButton() {}
+function playPause() { video[video.paused ? 'play' : 'pause'](); }  // easy version-> `video.paused ? video.play() : video.pause()`
+function updateButton() { playbackToggle.textContent = this.paused ? '►' : '❚❚'; }
+function skip() { video.currentTime += parseInt(this.dataset.skip, 10); }
+function sliderUpdate() { video[this.name] = this.value; }
 
 video.addEventListener('click', playPause);
 video.addEventListener('play', updateButton);
-video.addEventListener('paused', updateButton);
+video.addEventListener('pause', updateButton);
 
 playbackToggle.addEventListener('click', playPause);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+sliders.forEach(slider => slider.addEventListener('change', sliderUpdate));
+sliders.forEach(slider => slider.addEventListener('mouseMove', sliderUpdate));
